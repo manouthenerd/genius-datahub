f413722138e0d2dcdb05c2da82980f94d480d898
Leplusbio225 <196662423+manouthenerd@users.noreply.github.com>
2025-09-13 11:57:40 +0000

    style(ui): ajustement des propriétés SVG pour améliorer l'affichage et ajout d'un effet d'ombre portée

--- resources/js/components/ui/Folder.vue
+++ resources/js/components/ui/Folder.vue
@@ -1,6 +1,6 @@
 <template>
-    <svg width="75" height="75" xmlns="http://www.w3.org/2000/svg" fill="#ffd86b" viewBox="0 0 24 24" :stroke-width="1"
-        stroke="black" className="size-6">
+    <svg width="75" height="75" xmlns="http://www.w3.org/2000/svg" fill="#ffd86b" viewBox="0 0 24 24" :stroke-width="0.7"
+        stroke="#000000b5" className="size-6">
         <path strokeLinecap="round" strokeLinejoin="round"
             d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
     </svg>
@@ -10,6 +10,8 @@
 <style scoped>
 svg {
     transition: transform .2s ease;
+    filter: drop-shadow(1px -1px 0px #000);
+
 }
 svg:hover {
     transform: scale(1.1);
