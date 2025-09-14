b92adfc4a717ea783cca2b90bcf7fe299980dd7a
Leplusbio225 <196662423+manouthenerd@users.noreply.github.com>
2025-09-14 23:06:37 +0000

    feat(CreateProjectDialog): ajout de nouveaux champs pour la création de projets, y compris l'attribution de services pour les utilisateurs admin, et mise à jour des champs de formulaire pour une meilleure gestion des données.

--- resources/js/components/dialog/CreateProjectDialog.vue
+++ resources/js/components/dialog/CreateProjectDialog.vue
@@ -12,31 +12,44 @@
                 <DialogDescription> Ajouter un nouveau projet à votre liste de projets
                 </DialogDescription>
             </DialogHeader>
-            <Form :action="route('projects.create')" method="POST" v-slot="{ errors, processing }"
-                :reset-on-success="['name', 'email']">
+            <Form @success="alert.turnAlertOn" :action="route('projects.create')" method="POST"
+                v-slot="{ errors, processing }"
+                :reset-on-success="['name', 'email', 'tag', 'from', 'to', 'description']">
                 <div class="grid gap-4 py-2">
                     <div class="grid gap-1">
                         <Label for="title">Libellé du projet </Label>
-                        <Input required id="title" placeholder="Entrer un libellé pour votre projet" name="title" class="ring-transparent!  focus:border-slate-500!" />
+                        <Input required id="title" placeholder="Entrer un libellé pour votre projet" name="title"
+                            class="ring-transparent!  focus:border-slate-500!" />
                         <InputError :message="errors.title" />
                     </div>
                     <div class="grid gap-1">
                         <Label for="tag"> Tag du projet</Label>
-                        <Input required id="tag" name="tag" placeholder="ex: vidéosurveillance" class="ring-transparent!  focus:border-slate-500!" />
+                        <Input required id="tag" name="tag" placeholder="ex: vidéosurveillance"
+                            class="ring-transparent!  focus:border-slate-500!" />
                         <InputError :message="errors.tag" />
                     </div>
+                    <div v-if="userHasRole('admin')" class="grid gap-1">
+                        <Label for="service_id">Attribuer au service</Label>
+                        <SelectInput :required="userHasRole('admin')" name="service_id" v-model="selectedService" placeholder="Choix du service">
+                            <SelectItem v-for="service in services" :value="service.id" :key="service.id">
+                               {{ service.name }}
+                            </SelectItem>
+                        </SelectInput>
+                        <InputError :message="errors.service_id" />
+                    </div>
+                    <input v-else type="hidden" name="service_id" :value="service_id">
                     <div class="grid gap-1">
                         <Label for="priority">Niveau de priorité</Label>
 
                         <SelectInput required name="priority" placeholder="Choix du niveau de priorité">
-                            <SelectItem value="faible">
-                                faible
+                            <SelectItem value="low">
+                                basse
                             </SelectItem>
-                            <SelectItem value="normal">
+                            <SelectItem value="medium">
                                 moyenne
                             </SelectItem>
-                            <SelectItem value="élévé">
-                                élévé
+                            <SelectItem value="high">
+                                haute
                             </SelectItem>
                         </SelectInput>
                         <InputError :message="errors.priority" />
@@ -44,27 +57,26 @@
 
                     <div class="grid grid-cols-2 gap-2">
                         <div class="grid gap-1">
-                            <Label for="starting_date">Date de début du projet</Label>
-                            <Input required
-                                class="ring-transparent!  focus:border-slate-500!" type="date" name="starting_date" />
-                            <InputError :message="errors.starting_date" />
+                            <Label for="from">Date de début du projet</Label>
+                            <Input required class="ring-transparent!  focus:border-slate-500!" type="date"
+                                name="from" />
+                            <InputError :message="errors.from" />
                         </div>
 
                         <div class="grid gap-1">
-                            <Label for="ending_date">Date de fin du projet</Label>
-                            <Input required
-                                class="ring-transparent!  focus:border-slate-500!" type="date" name="ending_date" />
-                            <InputError :message="errors.ending_date" />
+                            <Label for="to">Date de fin du projet</Label>
+                            <Input required class="ring-transparent!  focus:border-slate-500!" type="date" name="to" />
+                            <InputError :message="errors.to" />
                         </div>
                     </div>
 
                     <div class="grid gap-1">
                         <Label for="description">Description du projet</Label>
 
-                        <Textarea class="ring-transparent!  focus:border-slate-500!" placeholder="Entrer la description ici." />
+                        <Textarea name="description" class="ring-transparent! focus:border-slate-500!"
+                            placeholder="Entrer la description ici." />
                         <InputError :message="errors.description" />
                     </div>
-
                 </div>
                 <DialogFooter>
                     <Button :disabled="processing" type="submit"
@@ -79,8 +91,8 @@
 </template>
 
 <script setup lang="ts">
-import { Form } from '@inertiajs/vue3';
-import { Plus } from 'lucide-vue-next';
+import { Form, usePage } from '@inertiajs/vue3';
+import { Plus, Loader } from 'lucide-vue-next';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import Input from '@/components/ui/input/Input.vue';
 import InputError from '@/components/InputError.vue';
@@ -89,5 +101,30 @@ import Label from '@/components/ui/label/Label.vue';
 import SelectItem from '@/components/ui/select/SelectItem.vue';
 import { Button } from '@/components/ui/button';
 import Textarea from "@/components/ui/textarea/Textarea.vue";
+import { useAlertStore } from '@/stores/alert';
+import { ref } from 'vue';
+
+const alert = useAlertStore()
+
+const selectedService = ref('');
+const user: User = usePage().props.auth.user;
+
+interface Service {
+    id: number,
+    name: string
+}
+
+interface User {
+    id: number,
+    name: string,
+    role?: string,
+    email?: string
+}
+
+function userHasRole(role: string): boolean {
+    return user.role == role;
+}
+
+defineProps<{services: Service[], service_id: number}>();
 
 </script>
