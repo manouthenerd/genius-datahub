ac24670ce6d273e82887fa9469ebfb474148c8fd
Leplusbio225 <196662423+manouthenerd@users.noreply.github.com>
2025-09-14 23:06:27 +0000

    feat(ProjectController): ajout de tests pour la visite de la page des projets et la création de projets avec validation des données, incluant la gestion des erreurs de session.

--- tests/Feature/Controllers/ProjectController/IndexProjectTest.php
+++ tests/Feature/Controllers/ProjectController/IndexProjectTest.php
@@ -1,9 +1,33 @@
 <?php
 
-// use App\Models\User;
+use App\Models\User;
 
-// it('shows some words on the page', function () {
-//     $user = User::factory()->create();
 
-//     $page = visit('/tasks');
-// });
+
+describe('can performs some actions on projects and tasks page', function () {
+
+    beforeEach(function () {
+        $this->user = User::factory()->create(['role' => 'moderator']);
+    });
+
+    it('can visit projects', function () {
+
+        $response = $this->actingAs($this->user)->get('/tasks');
+
+        $response->assertStatus(200);
+    });
+
+    it('session has no errors', function() {
+        $response = $this->actingAs($this->user)->post('/projects', [
+            'title' => 'Titre du projet',
+            'tag' => 'vidéosurveillance',
+            'priority' => 'high',
+            'service_id' => $this->user->service_id,
+            'from' => now(),
+            'to' => now()->addHour(),
+            'description' => 'Description du projet'
+        ]);
+
+        $response->assertValid();;
+    });
+});
