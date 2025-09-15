12676bd1aad72e272086bb559f1210733dd9e0ab
Leplusbio225 <196662423+manouthenerd@users.noreply.github.com>
2025-09-15 00:18:46 +0000

    feat(ProjectController, UpdateProjectRequest): ajout des méthodes de mise à jour et de suppression des projets, ainsi que de la validation des données pour la mise à jour des projets.

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
 use App\Models\Project;
-use Illuminate\Http\Request;
-use Inertia\Inertia;
 
 class ProjectController extends Controller
 {
@@ -15,9 +14,23 @@ class ProjectController extends Controller
         $validated = $request->validated();
 
         // Création du projet
-        $project = Project::create($validated);
+        Project::create($validated);
 
         return redirect()->back()
             ->with('success', 'Projet créé avec succès !');
+    }
+
+    public function update(UpdateProjectRequest $request, Project $project) {
+
+        $project->update($request->validated());
+
+        return redirect()->back();
+    }
+
+    public function destroy(Project $project) {
+        
+        $project->delete();
+
+        return redirect()->back();
     }
 }
