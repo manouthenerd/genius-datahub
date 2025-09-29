092b340f73799e2d5ecfaf1473c8afebf5dd39bd
Leplusbio225 <196662423+manouthenerd@users.noreply.github.com>
2025-09-29 06:21:51 +0000

    feat(TaskController): ajout des méthodes de création, mise à jour, édition et suppression des tâches, avec des vérifications de rôle pour la sécurité et amélioration de la récupération des projets et services associés.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,32 +2,92 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Project;
 use Inertia\Inertia;
 use App\Models\Service;
 use App\Models\Task;
-use App\Models\User;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index() {
+    public function index()
+    {
 
         $user = auth()->user();
 
         return Inertia::render('Task', [
             'projects' => Project::all(),
             'tasks' => Task::all(),
-            'services' => Service::all(['id', 'name']), 
+            'services' => Service::all(['id', 'name']),
             'service_id' => $user->service_id ?? NULL,
         ]);
     }
 
-    public function create() {
+    public function create()
+    {
+
         return Inertia::render('CreateTask', [
-            'projects' => Project::all(['id', 'name']),
+            'projects' => Project::with('service')->get(),
         ]);
     }
 
-    
+    public function store(CreateTaskRequest $request)
+    {
+        $validated = $request->validated();
+
+        $task = Task::create($validated);
+
+        return redirect()->back();
+    }
+
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $validated = $request->validated();
+
+        $task->update($validated);
+
+        return redirect()->back();
+    }
+
+    public function edit(Request $request, Task $task)
+    {   
+
+        $user = $request->user();
+
+        $project = Project::with('service')->find($task->project_id);
+
+        $service = $project->service;
+
+        $service_users = $service->users()->get();
+
+        if (!($user->role === 'admin' || ($user->role === 'moderator' && $service->id === $user->service_id))) {
+            return abort(403);
+        }
+
+        return Inertia::render('EditTask', [
+            'project' => $project,
+            'task' => $task,
+            'users' => $service_users
+        ]);
+    }
+
+    public function destroy(Task $task)
+    {
+
+        $user = request()->user();
+
+        $project = Project::with('service')->find($task->project_id);
+
+        $service = $project->service;
+
+        if (!($user->role === 'admin' || ($user->role === 'moderator' && $service->id === $user->service_id))) {
+            return abort(403);
+        }
+
+        $task->delete();
+
+        return redirect('/tasks');
+    }
 }
