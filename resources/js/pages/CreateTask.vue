6a0a2ae53202c5ee842995ee0482094468d4cf08
Leplusbio225 <196662423+manouthenerd@users.noreply.github.com>
2025-10-11 22:20:19 +0000

    feat(CreateTask): enhance task creation form with user delegation and dynamic project service updates

--- resources/js/pages/CreateTask.vue
+++ resources/js/pages/CreateTask.vue
@@ -40,13 +40,28 @@
                         <div class="grid gap-1">
                             <Label for="projet_id">Ajouter au projet:</Label>
 
-                            <select required class="ring-transparent! px-3 border border-input h-[40px] bg-white rounded-md shadow-xs focus:border-slate-500" name="project_id" v-model="selectedProject">
+                            <select @change="updateUsersAndCurrentProjectService(currentProjectService()?.service.id)" required class="ring-transparent! px-3 border border-input h-[40px] bg-white rounded-md shadow-xs focus:border-slate-500" name="project_id" v-model="selectedProject">
                                 
                                 <option class="text-black" v-for="project in projects" :key="project.id" :value="project.id">
                                    {{ project.title }} ({{ project.service.name }})
                                 </option>
                                
                             </select>
+
+                            <input type="hidden" name="service_id" :value="currentProjectService()?.service.id">
+                            
+                            <InputError :message="errors.project_id" />
+                        </div>
+
+                        <div class="grid gap-1">
+                            <Label for="projet_id">Déléguer la tâche à:</Label>
+
+                            <div class="flex flex-wrap w-full p-2 gap-4 border-slate-100 min-h-[100px] inset-1  shadow-inner rounded bg-white focus:outline-0">
+                                <label v-for="user in users" :key="user.id" :for="`user${user.id}`">
+                                    <input name="users[]" v-model="selectedUsers" :value="user.id" type="checkbox" :id="`user${user.id}`"> {{ user.name }}
+                                </label>
+                                
+                            </div>
                             
                             <InputError :message="errors.project_id" />
                         </div>
@@ -98,9 +113,10 @@ import Label from '@/components/ui/label/Label.vue';
 import { Button } from '@/components/ui/button';
 import Textarea from "@/components/ui/textarea/Textarea.vue";
 import { useAlertStore } from '@/stores/alert';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { type BreadcrumbItem } from '@/types';
 import AppLayout from '@/layouts/AppLayout.vue';
+import { useFetch } from '@vueuse/core';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -120,14 +136,29 @@ interface Project {
     }
 }
 
-interface User {
-    id: number,
-    name: string,
-    role?: string,
-    email?: string
+const users = ref([])
+
+const selectedUsers = ref([])
+
+const props = defineProps<{ projects: Project[]}>();
+
+const selectedProject = ref(props.projects.length ? props.projects[0].id : "");
+
+const currentProjectService = () => props.projects.find(project => project.id == selectedProject.value)
+
+onMounted(() =>  {
+    getUsers(currentProjectService()?.service.id);
+})
+
+function getUsers(id: any) {
+    useFetch(route('task-users', {service: id}))
+    .then(response => users.value = response.data)
+    .then((data) => users.value = JSON.parse(data.value))
 }
 
-const props = defineProps<{ projects: Project[], users: User[], service_id: number }>();
-
-const selectedProject = ref(props.projects.length ? props.projects[0].id : '');
+const updateUsersAndCurrentProjectService = (id: number) => {
+    currentProjectService()
+    getUsers(id)
+    selectedUsers.value.length = 0
+}
 </script>
