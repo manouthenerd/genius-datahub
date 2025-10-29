3565dd4d2dbdfbc4dcab2b0eeea2c5141a3646fd
Leplusbio225 <196662423+manouthenerd@users.noreply.github.com>
2025-10-29 11:58:01 +0000

    refactor(password): enhance password settings page with French translations and alert functionality for successful updates

--- resources/js/pages/settings/Password.vue
+++ resources/js/pages/settings/Password.vue
@@ -10,6 +10,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { type BreadcrumbItem } from '@/types';
+import { useAlertStore } from '@/stores/alert';
 
 const breadcrumbItems: BreadcrumbItem[] = [
     {
@@ -20,17 +21,23 @@ const breadcrumbItems: BreadcrumbItem[] = [
 
 const passwordInput = ref<HTMLInputElement | null>(null);
 const currentPasswordInput = ref<HTMLInputElement | null>(null);
+
+const alert = useAlertStore()
+
+alert.message = "Modification effectuée avec succès";
+
 </script>
 
 <template>
     <AppLayout :breadcrumbs="breadcrumbItems">
-        <Head title="Password settings" />
+        <Head title="Paramètres du mot de passe" />
 
         <SettingsLayout>
             <div class="space-y-6">
-                <HeadingSmall title="Update password" description="Ensure your account is using a long, random password to stay secure" />
+                <HeadingSmall title="Mise à jour du mot de passe" description="Assurez-vous de définir un mot de passe fort et sécurisé pour votre compte" />
 
                 <Form
+                    @success="alert.turnAlertOn"
                     method="put"
                     :action="route('password.update')"
                     :options="{
@@ -39,60 +46,51 @@ const currentPasswordInput = ref<HTMLInputElement | null>(null);
                     reset-on-success
                     :reset-on-error="['password', 'password_confirmation', 'current_password']"
                     class="space-y-6"
-                    v-slot="{ errors, processing, recentlySuccessful }"
+                    v-slot="{ errors, processing }"
                 >
                     <div class="grid gap-2">
-                        <Label for="current_password">Current password</Label>
+                        <Label for="current_password">Mot de passe actuel</Label>
                         <Input
                             id="current_password"
                             ref="currentPasswordInput"
                             name="current_password"
                             type="password"
-                            class="mt-1 block w-full"
+                            class="mt-1 block w-full bg-white"
                             autocomplete="current-password"
-                            placeholder="Current password"
+                            placeholder="Mot de passe actuel"
                         />
                         <InputError :message="errors.current_password" />
                     </div>
 
                     <div class="grid gap-2">
-                        <Label for="password">New password</Label>
+                        <Label for="password">Nouveau mot de passe</Label>
                         <Input
                             id="password"
                             ref="passwordInput"
                             name="password"
                             type="password"
-                            class="mt-1 block w-full"
+                            class="mt-1 block w-full bg-white"
                             autocomplete="new-password"
-                            placeholder="New password"
+                            placeholder="Nouveau mot de passe"
                         />
                         <InputError :message="errors.password" />
                     </div>
 
                     <div class="grid gap-2">
-                        <Label for="password_confirmation">Confirm password</Label>
+                        <Label for="password_confirmation">Confirmer le nouveau mot de passe</Label>
                         <Input
                             id="password_confirmation"
                             name="password_confirmation"
                             type="password"
-                            class="mt-1 block w-full"
+                            class="mt-1 block w-full bg-white"
                             autocomplete="new-password"
-                            placeholder="Confirm password"
+                            placeholder="Confirmer le nouveau mot de passe"
                         />
                         <InputError :message="errors.password_confirmation" />
                     </div>
 
                     <div class="flex items-center gap-4">
-                        <Button :disabled="processing">Save password</Button>
-
-                        <Transition
-                            enter-active-class="transition ease-in-out"
-                            enter-from-class="opacity-0"
-                            leave-active-class="transition ease-in-out"
-                            leave-to-class="opacity-0"
-                        >
-                            <p v-show="recentlySuccessful" class="text-sm text-neutral-600">Saved.</p>
-                        </Transition>
+                        <Button variant="default" class="bg-[#0074B8] hover:bg-[#0074B8] hover:opacity-90" :disabled="processing">Enregistrer</Button>
                     </div>
                 </Form>
             </div>
