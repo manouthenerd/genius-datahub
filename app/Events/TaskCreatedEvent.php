bbc7e9c9df5ffa9f784d331bcc238e5a2e105772
Leplusbio225 <196662423+manouthenerd@users.noreply.github.com>
2025-10-29 11:12:09 +0000

    refactor(archive): streamline ArchiveController by optimizing response handling and removing redundant code

---
+++ app/Events/TaskCreatedEvent.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Task;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class TaskCreatedEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $task;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(Task $task)
+    {
+        $this->task = $task;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('tasks'),
+        ];
+    }
+
+    public function broadcastAs(): string{
+        return 'task.created';
+    }
+}
