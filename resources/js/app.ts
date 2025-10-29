6cbf948ed488885b62e47e9634b67badc3fd329c
Leplusbio225 <196662423+manouthenerd@users.noreply.github.com>
2025-10-29 11:10:09 +0000

    feat(broadcast): integrate Pusher for real-time task updates and configure Echo settings

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -5,20 +5,29 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
-import { configureEcho } from '@laravel/echo-vue';
-import { configureEcho } from '@laravel/echo-vue';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
-configureEcho({
+import { createPinia } from 'pinia';
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
     broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST ?? '127.0.0.1',
+    wsPort: 9001,
+    wssPort: 9001,
+    forceTLS: false,
+    enabledTransports: ['ws'],
 });
 
-configureEcho({
-    broadcaster: 'reverb',
+window.Echo.channel('tasks').listen('.task.created', (e) => {
+    console.log('Nouvelle tâche reçue :', e);
 });
-import { createPinia } from 'pinia'
 const appName = import.meta.env.VITE_APP_NAME || 'Genius DataHub';
 
-const pinia = createPinia()
+const pinia = createPinia();
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
